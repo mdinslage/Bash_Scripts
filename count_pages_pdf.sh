50f6ffbf59832b8ec1e9b13dfa10a2885a78adbb
Matt Dinslage <matthewdinslage@gmail.com>
2023-10-19 12:02:32 -0500

    Change to make .pdf extension case insensitive

--- count_pages_pdf.sh
+++ count_pages_pdf.sh
@@ -2,14 +2,17 @@
 
 total_pages=0
 
-# Loop through each PDF file in the directory
+# Loop through each PDF file in the directory (case-insensitive)
+shopt -s nocaseglob
 for f in *.pdf; do
-  pages=$(pdfinfo "$f" | grep Pages | awk '{print $2}')
-  echo "File: $f, Pages: $pages"
-  total_pages=$((total_pages + pages))
+  if [[ -f "$f" ]]; then
+    pages=$(pdfinfo "$f" | grep Pages | awk '{print $2}')
+    echo "File: $f, Pages: $pages"
+    total_pages=$((total_pages + pages))
+  fi
 done
+shopt -u nocaseglob  # Reset case-insensitive option
 
 # Print the total number of pages
 echo -e "\033[32mTotal Pages: $total_pages\033[0m"
-
 
