05f514528fe95ac31fddc5f7e1b15d2cc3411aec
Matt Dinslage <matthewdinslage@gmail.com>
2024-01-13 01:24:59 -0600

    add architecture check

--- get_flatpak.sh
+++ get_flatpak.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ -z "$ARCH" ]; then
+  case "$( uname -m )" in
+    i?86) ARCH=i586 ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=$( uname -m ) ;;
+  esac
+fi
+
+# Check architecture
+if [ "$ARCH" = "i586" ]; then
+  LIBDIRSUFFIX=""
+elif [ "$ARCH" = "i686" ]; then
+  LIBDIRSUFFIX=""
+else [ "$ARCH" = "x86_64" ]
+  LIBDIRSUFFIX="64"
+fi
+
 # Ask for Slackware verson
 read -p "Do you want packages for 15.0 or current? " user_input
 
@@ -18,6 +35,6 @@ mkdir -p /tmp/flatpak/$SLACKVER
 cd /tmp/flatpak/$SLACKVER
 
 for i in flatpak appstream bubblewrap libostree xdg-dbus-proxy xdg-desktop-portal-gtk ; do
-  wget -r -np -nd -l1 --accept=*.t?z http://www.slackware.com/~alien/slackbuilds/$i/pkg64/$SLACKVER/
+  wget -r -np -nd -l1 --accept=*.t?z http://www.slackware.com/~alien/slackbuilds/$i/pkg$LIBDIRSUFFIX/$SLACKVER/
 done
 
