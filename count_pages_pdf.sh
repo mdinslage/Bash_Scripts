51ccb16c82508bf7b6b38a3cb2f21701c553678d
Matt Dinslage <matthewdinslage@gmail.com>
2024-05-31 15:04:06 -0500

    supress error messages
    suppress error messages, Print a warning message for files where the page count could not be determined

--- count_pages_pdf.sh
+++ count_pages_pdf.sh
@@ -6,13 +6,16 @@ total_pages=0
 shopt -s nocaseglob
 for f in *.pdf; do
   if [[ -f "$f" ]]; then
-    pages=$(pdfinfo "$f" | grep Pages | awk '{print $2}')
-    echo "File: $f, Pages: $pages"
-    total_pages=$((total_pages + pages))
+    pages=$(pdfinfo "$f" 2>/dev/null | grep Pages | awk '{print $2}')
+    if [[ -n "$pages" ]]; then
+      echo "File: $f, Pages: $pages"
+      total_pages=$((total_pages + pages))
+    else
+      echo -e "\033[31mWarning: Could not get page count for file: $f\033[0m"
+    fi
   fi
 done
 shopt -u nocaseglob  # Reset case-insensitive option
 
 # Print the total number of pages
 echo -e "\033[32mTotal Pages: $total_pages\033[0m"
-
