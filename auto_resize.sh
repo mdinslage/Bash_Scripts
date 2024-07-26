368a856d533c13ab82f849a67ea0582a28b3a493
Matt Dinslage <matthewdinslage@gmail.com>
2024-07-26 02:04:12 -0500

    Add DRM card detection

--- auto_resize.sh
+++ auto_resize.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-# XFce does not support auto resize of 
-#the guest window. this script will 
+#XFce does not support auto resize of
+#the guest window. This script will
 #enable that feature.
 
-# Check if the user is root
+# Check if the script is run as root
 if [ "$(whoami)" != "root" ]; then
   echo "Please run as root"
   exit 1
 fi
 
-# Check if the spice-vdagent file exists
+# Check if spice-vdagent is installed on Slackware
 if [ -f /etc/slackware-version ]; then
-  if ! [ -e /var/lib/pkgtools/packages/spice-vdagent* ]; then
+  if ! ls /var/lib/pkgtools/packages/spice-vdagent* >/dev/null 2>&1; then
     echo "Please install spice-vdagent first"
     exit 1
   fi
 fi
 
-# Debian family system check
+# Install spice-vdagent on Debian-based systems
 if [ -f /etc/debian_version ]; then
   if ! dpkg -s spice-vdagent >/dev/null 2>&1; then
     echo "Installing spice-vdagent..."
-    apt-get update
-    apt-get install -y spice-vdagent
+    apt-get update && apt-get install -y spice-vdagent
   fi
 fi
 
-#RedHat famiy system check
+# Install spice-vdagent on RedHat-based systems
 if [ -f /etc/redhat-release ]; then
   if ! rpm -q spice-vdagent >/dev/null 2>&1; then
     echo "Installing spice-vdagent..."
@@ -35,23 +34,30 @@ if [ -f /etc/redhat-release ]; then
   fi
 fi
 
-# Create the udev rule file
-echo 'ACTION=="change",KERNEL=="card0",SUBSYSTEM=="drm",RUN+="/usr/local/bin/x-resize"' > /etc/udev/rules.d/50-x-resize.rules
+# Find the active DRM device
+card_device=$(ls /sys/class/drm/ | grep -E 'card[0-9]+' | head -n 1 | sed 's/@//')
+if [ -z "$card_device" ]; then
+  echo "No DRM device found. Exiting."
+  exit 1
+fi
+
+# Create the udev rule file with the correct card device
+echo "ACTION==\"change\", KERNEL==\"$card_device\", SUBSYSTEM==\"drm\", RUN+=\"/usr/local/bin/x-resize\"" > /etc/udev/rules.d/50-x-resize.rules
 
 # Create the x-resize script
-echo '#! /bin/sh 
+cat <<'EOF' > /usr/local/bin/x-resize
+#!/bin/sh
 PATH=/usr/bin
-desktopuser=$(/bin/ps -ef  | /bin/grep -oP "^\w+ (?=.*vdagent( |$))") || exit 0
+desktopuser=$(ps -ef | grep -oP "^\w+ (?=.*vdagent( |$))") || exit 0
 export DISPLAY=:0
 export XAUTHORITY=$(eval echo "~$desktopuser")/.Xauthority
-xrandr --output $(xrandr | awk "/ connected/{print \$1; exit; }") --auto' > /usr/local/bin/x-resize
+xrandr --output $(xrandr | awk "/ connected/{print \$1; exit; }") --auto
+EOF
 
-# Make file executable
+# Make the x-resize script executable
 chmod +x /usr/local/bin/x-resize
 
 # Reload the udev rules
 udevadm control --reload-rules
 
-echo "Finished, logout or reboot may be required"
-
-
+echo "Setup complete. A logout or reboot may be required."
